2497539717bfc134e003fc820f3fe15f9c296840
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-13 14:01:01 -0400

    AddADram - created function handlePost to POST newDram

--- src/components/AddADram/AddADram.jsx
+++ src/components/AddADram/AddADram.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import useWhiskey from "../../hooks/useWhiskey";
+import { useDispatch } from "react-redux";
 
 function AddADram(){
+    //dispatch
+    const dispatch = useDispatch();
+
     //state var
-    const [newDram, setNewDram] = useState({name: '', proof: '', quantity: '', calories: ''});
+    const [newDram, setNewDram] = useState({name: '', proof: '', quantity: '', calories: '', whiskeyExists: false, whiskeyID: ''});
+
+    //array of whiskies
+    const whiskeyArray = useWhiskey();
 
     //function calCals to calculate calories given user input
     const calcCals = (event) => {
         event.preventDefault();
-        
         //input validation here-ensure user inputted numbers into proof/quantity fields. Also ensure proof <= 200
         const validation = validateNums();
         if (validation) {
@@ -18,6 +25,26 @@ function AddADram(){
             setNewDram({...newDram, calories: parseInt(dramCals)});
         } else{
             alert('Please enter valid proof and quantity values');
+            return;
+        } 
+    }
+
+    //function handlePost - checks if whiskey name/proof exists in whiskeyArray to finalize post payload
+    const handlePost = () => {
+        //check - user must have entered info and calculated to POST
+        if (!newDram.calories) {
+            alert('Please enter dram information and calculate');
+            return;
+        } else {
+            //if newDram whiskey info matches entry already in DB, change exists property to true and change whiskeyID to whiskey.id
+            for (let whiskey of whiskeyArray) {
+                if (whiskey.whiskey_name == newDram.name && whiskey.whiskey_proof == newDram.proof) {
+                    setNewDram({...newDram, whiskeyExists: true});
+                    setNewDram({...newDram, whiskeyID: whiskey.id});
+                }
+            }
+            dispatch({type: 'ADD_NEW_DRAM', payload: newDram});
+            //after dispatch, route user to view drams component
         }
     }
 
@@ -37,7 +64,7 @@ function AddADram(){
 
     const clearInputs = () => {
         //consider validation sweetAlert - ensure user wants to clear
-        setNewDram({name: '', proof: '', quantity: '', calories: ''});
+        setNewDram({name: '', proof: '', quantity: '', calories: '', whiskeyExists: false, whiskeyID: ''});
     }
 
     return(
@@ -48,10 +75,10 @@ function AddADram(){
                 <input required type="number" placeholder="whiskey proof" value={newDram.proof} onChange={(event) => setNewDram({...newDram, proof: event.target.value})}/>
                 <input required type="number" placeholder="Oz whiskey" value={newDram.quantity} onChange={(event) => setNewDram({...newDram, quantity: event.target.value})}/>
                 <button type="submit">Calculate Calories</button>
-                <button>Add this dram</button>
                 <button onClick={clearInputs}>Clear form</button>
-                <p>Calories: {newDram.calories}</p>
             </form>
+            <p>Calories: {newDram.calories}</p>
+            <button onClick={handlePost}>Add this dram</button>
         </>
     );
 }
