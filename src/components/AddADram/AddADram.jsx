cbc0bd76498acd79948b7e97917e566dd9fde9bb
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-13 11:51:44 -0400

    updated AddADram with input validation

--- src/components/AddADram/AddADram.jsx
+++ src/components/AddADram/AddADram.jsx
@@ -7,12 +7,32 @@ function AddADram(){
     //function calCals to calculate calories given user input
     const calcCals = (event) => {
         event.preventDefault();
-        //input validation here-ensure user inputted numbers into proof/quantity fields
-        const ozAlc = (newDram.proof*newDram.quantity)/200;
-        const mLAlc = 29.5735*ozAlc;
-        const gAlc = 0.789*mLAlc;
-        const dramCals = 7*gAlc;
-        setNewDram({...newDram, calories: parseInt(dramCals)});
+        
+        //input validation here-ensure user inputted numbers into proof/quantity fields. Also ensure proof <= 200
+        const validation = validateNums();
+        if (validation) {
+            const ozAlc = (newDram.proof*newDram.quantity)/200;
+            const mLAlc = 29.5735*ozAlc;
+            const gAlc = 0.789*mLAlc;
+            const dramCals = 7*gAlc;
+            setNewDram({...newDram, calories: parseInt(dramCals)});
+        } else{
+            alert('Please enter valid proof and quantity values');
+        }
+    }
+
+    //function to validate inputted numbers
+    const validateNums = () => {
+        //inputted nums should not be negative
+        if (newDram.proof < 0 || newDram.quantity < 0) {
+            return false;
+        }
+        //proof cannot exceed 200 
+        if (newDram.proof > 200) {
+            return false;
+        }
+        else 
+            return true
     }
 
     const clearInputs = () => {
@@ -29,7 +49,7 @@ function AddADram(){
                 <input required type="number" placeholder="Oz whiskey" value={newDram.quantity} onChange={(event) => setNewDram({...newDram, quantity: event.target.value})}/>
                 <button type="submit">Calculate Calories</button>
                 <button>Add this dram</button>
-                <button onClick={() => clearInputs}>Clear form</button>
+                <button onClick={clearInputs}>Clear form</button>
                 <p>Calories: {newDram.calories}</p>
             </form>
         </>
