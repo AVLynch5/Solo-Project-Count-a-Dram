bd63578fdf655497c9c495c230cee3ca64f4f0b3
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-13 14:54:36 -0400

    changed /api/dram to /api/whiskey

--- src/redux/sagas/whiskey.saga.js
+++ src/redux/sagas/whiskey.saga.js
@@ -4,7 +4,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 // worker Saga: will be fired on "FETCH_USER" actions
 function* fetchWhiskey() {
   try {
-    const response = yield axios.get('/api/dram');
+    const response = yield axios.get('/api/whiskey');
     yield put({ type: 'SET_WHISKIES', payload: response.data });
   } catch (error) {
     console.log('Whiskies GET request error', error);
