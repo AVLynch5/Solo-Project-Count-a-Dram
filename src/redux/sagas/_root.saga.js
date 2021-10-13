0761eabff84851c454b7ce56a0063285dd59dc79
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-13 16:04:52 -0400

    added dramSaga to rootSaga

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,6 +3,7 @@ import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 import whiskeySaga from './whiskey.saga';
+import dramSaga from './dram.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -17,5 +18,6 @@ export default function* rootSaga() {
     registrationSaga(),
     userSaga(),
     whiskeySaga(),
+    dramSaga(),
   ]);
 }
