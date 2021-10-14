be2d964145d3ce7659ea514d8520d59a796e1dd6
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-14 14:57:17 -0400

    added calendar and moment to ViewDrams

--- src/components/ViewDrams/ViewDrams.jsx
+++ src/components/ViewDrams/ViewDrams.jsx
@@ -1,4 +1,34 @@
 import React, { useState } from "react";
 //import the calendar
 import Calendar from "react-calendar";
+//import momentjs
+import moment from "moment";
 
+function ViewDrams() {
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue) {
+        setValue(nextValue);
+        const dateToDisplay = moment(nextValue).format('l');
+        alert(`The date selected is ${dateToDisplay}`);
+    }
+
+    return(
+        <>
+            <div className="calendar">
+                <h3>Pick a Date</h3>
+                <div className="calContainer">
+                    <Calendar 
+                        onChange={onChange}
+                        value={value}
+                    />
+                </div>
+            </div>
+            <div className="dramData">
+                <h3>Drams from {value}</h3>
+            </div>
+        </>
+    );
+}
+
+export default ViewDrams;
