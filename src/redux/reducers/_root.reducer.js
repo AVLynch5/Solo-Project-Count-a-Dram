756c41773bb6d7273f4a50ed783242b0627f4dc2
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-15 17:58:14 -0400

    deleted whiskeyReducer and removed from rootReducer

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -13,7 +13,6 @@ import dram from './dram.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
-  whiskey, //will have an id, proof, and optionally whiskey name
   dram,
 });
 
