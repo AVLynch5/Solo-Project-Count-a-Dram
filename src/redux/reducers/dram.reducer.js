733e2b11962d6d7afa91efd1283922e9608396de
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-14 22:40:13 -0400

    added extra actions to dramReducer

--- src/redux/reducers/dram.reducer.js
+++ src/redux/reducers/dram.reducer.js
@@ -1,9 +1,15 @@
 const dramReducer = (state = [], action) => {
     switch (action.type) {
-      case 'SET_DRAMS':
-        return action.payload;
-      default:
-        return state;
+        case 'SET_DRAMS':
+            return action.payload;
+        case 'EDIT_WHISKEY_NAME':
+            return [...state, state[action.payload.index].whiskey_name = action.payload.name];
+        case 'EDIT_WHISKEY_PROOF':
+            return [...state, state[action.payload.index].whiskey_proof = action.payload.proof];
+        case 'EDIT_DRAM_QUANTITY':
+            return [...state, state[action.payload.index].dram_quantity = action.payload.quantity];
+        default:
+            return state;
     }
   };
   
