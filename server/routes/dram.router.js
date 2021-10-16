699e36630da9205bbb207aa0739540ca5cb44893
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-15 21:17:38 -0400

    updated dram.router with PUT route

--- server/routes/dram.router.js
+++ server/routes/dram.router.js
@@ -89,6 +89,7 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
 //DELETE dram with ID
 router.delete('/:id', rejectUnauthenticated, (req, res) => {
     const dramID = req.params.id;
+    //userID kind of unnecessary because only the user's drams are GETted and displayed
     const userID = req.user.id;
     const queryText = `DELETE FROM "dram" WHERE ("id" = $1 AND "user_id" = $2);`;
     pool.query(queryText, [dramID, userID])
@@ -99,6 +100,70 @@ router.delete('/:id', rejectUnauthenticated, (req, res) => {
             console.log('Error DELETEing data', error);
             res.sendStatus(500);
         });
+})
+
+//PUT - edit dram with dramID using info from payload
+router.put('/:id', rejectUnauthenticated, (req, res) => {
+    const dramID = req.params.id;
+    //Phase 1: Check "whiskey" for entries matching the user's inputted information
+    const searchQuery = `SELECT * FROM "whiskey" WHERE ("whiskey_name" = $1 AND "whiskey_proof" = $2);`;
+    pool.query(searchQuery, [req.body.whiskey_name, req.body.whiskey_proof])
+        .then((result) => {
+            //if result.rows.length == 0, whiskey does not exist. If result.rows.length == 1, whiskey does exist
+            const whiskeyExists = (result.rows.length == 1 ? true : false);
+            //Phase 2: If whiskeyExists false, must POST user inputted info into "whiskey" and return id, then POST dram info. If whiskeyExists true, can pull id from result.rows and POST dram info.
+            switch (whiskeyExists) {
+                case false:
+                    const insertWhiskeyQuery = `
+                        INSERT INTO "whiskey" ("whiskey_name", "whiskey_proof")
+                        VALUES ($1, $2)
+                        RETURNING "id";`;
+                    pool.query(insertWhiskeyQuery, [req.body.whiskey_name, req.body.whiskey_proof])
+                        .then((result) => {
+                            console.log(result.rows[0].id);
+                            const whiskeyID = result.rows[0].id;
+                            console.log(whiskeyID, 'yeeeeaaaaaa');
+                            //Here's where whiskeyID is used to PUT dram info
+                            const putDramQuery = `
+                                UPDATE "dram" SET "whiskey_id" = $1, "dram_quantity" = $2, "dram_calories" = $3
+                                WHERE "id" = $4
+                                VALUES ($1, $2, $3, $4);`;
+                            pool.query(putDramQuery, [whiskeyID, req.body.dram_quantity, req.body.dram_calories, dramID])
+                                .then((result) => {
+                                    res.sendStatus(201);
+                                })
+                                .catch((error) => {
+                                    console.log('Error editing dram', error);
+                                    res.sendStatus(500);
+                                });
+                        })
+                        .catch((error) => {
+                            console.log('Error creating new whiskey', error);
+                        })
+                    break;
+                case true:
+                    //Since whiskeyExists true, can pull existing id from matching object @ result.rows.id
+                    const whiskeyID = result.rows[0].id;
+                    console.log(whiskeyID, 'yeeeeaaaaaa');
+                    //Here's where whiskeyID is used to PUT dram info
+                    const putDramQuery = `
+                        UPDATE "dram" SET "whiskey_id" = $1, "dram_quantity" = $2, "dram_calories" = $3
+                        WHERE "id" = $4
+                        VALUES ($1, $2, $3, $4);`;
+                    pool.query(putDramQuery, [whiskeyID, req.body.dram_quantity, req.body.dram_calories, dramID])
+                        .then((result) => {
+                            res.sendStatus(201);
+                        })
+                        .catch((error) => {
+                            console.log('Error editing dram', error);
+                            res.sendStatus(500);
+                        });
+                    break; 
+            }
+        }) 
+        .catch((error) => {
+            console.log('Error GETting whiskey info from DB', error);
+        });    
 })
 
 module.exports = router;
