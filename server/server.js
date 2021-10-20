15b1bbe3825f7e22201eda4684b38cbc8d42726e
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-20 17:51:58 -0400

    deleted whiskey.router.js and removed whiskeyRouter from server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-const whiskeyRouter = require('./routes/whiskey.router');
 const dramRouter = require('./routes/dram.router');
 
 // Body parser middleware
@@ -25,7 +24,6 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/whiskey', whiskeyRouter);
 app.use('/api/dram', dramRouter);
 
 // Serve static files
