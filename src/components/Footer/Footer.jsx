84f926b0b84037a79bcbe5fc160e70e682440bd0
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-21 11:51:04 -0400

    modified footer and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return <footer>&copy; Anthony Lynch 2021</footer>;
 }
 
 export default Footer;
