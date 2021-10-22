cabf8af88c64e85640fd2564b35c5d854158ec3d
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-22 17:52:01 -0400

    updated about page text

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -23,7 +23,7 @@ function AboutPage() {
                 <h3>Welcome to Count A Dram!</h3>
                 <ul>
                   <li>To get started, click 'Count A Dram' in the upper left-hand corner to register. You can still use the calorie calculator in 'Add A Dram' without logging in!</li>
-                  <li>Click 'Add A Dram' to calculate the calories in a dram of whiskey. If you're logged in, you can 'register' your dram to be viewed and analyzed later</li>
+                  <li>Click 'Add A Dram' to calculate the calories in a dram of whiskey. If you're logged in, you can 'register' your dram to be viewed and analyzed later.</li>
                   <li>Click 'View Drams' to view your 'registered' drams. Click on a date to view any drams registered on that date. Drams can be edited and/or deleted!</li>
                   <li>Click 'Analyze Drams' to view your 'registered' dram data in a bar chart format. Click two dates on the calendar then click 'plot data' to display the sum calorie and dram data for each date between the selected dates in the bar chart.</li>
                   <li>Sláinte!</li>
