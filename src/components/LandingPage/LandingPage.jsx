ec4e9d675b2fcdd06b4ed5d57f4d58603cd550fd
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-22 17:45:15 -0400

    edited landingpage styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -21,7 +21,7 @@ function LandingPage() {
         <h2>{heading}</h2>
         <Grid container>
           <Grid item lg={9} md={6} sm={12} xs={12}>
-            <div>
+            <div className="toasts">
               <p>"We come into the world naked and bare, We go through the world with trouble and care, We depart from the world to we know not where, But if you're a thoroughbred here, you're a thoroughbred there." - Dan Jenkins</p>
               <p>”Here’s to lying, cheating, stealing, and drinking…If you’re going to lie, lie for a friend. If you’re going to cheat, cheat death. If you’re going to steal, steal a heart. If you’re going to drink, drink with me.”</p>
               <p>“There are good ships, and there are wood ships, ships that sail the sea, but the best ships are friendships, and may they always be.”</p>
