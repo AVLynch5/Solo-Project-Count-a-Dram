d1166b9f9758c0fcb4bd32d070492cc4775aa0bf
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-22 14:34:17 -0400

    further updated user page and landing page styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 import { Paper } from '@mui/material';
+import { Grid } from '@mui/material';
 
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
@@ -15,31 +16,34 @@ function LandingPage() {
   };
 
   return (
-    <Paper className="pageContainer" elevation={10} sx={{backgroundColor: '#F09F41'}}>
-    <div className="container">
-      <h2>{heading}</h2>
+    <Paper className="pageContainer" elevation={10} sx={{ backgroundColor: '#F09F41' }}>
+      <div className="container">
+        <h2>{heading}</h2>
+        <Grid container>
+          <Grid item lg={9} md={6} sm={12} xs={12}>
+            <div>
+              <p>"We come into the world naked and bare, We go through the world with trouble and care, We depart from the world to we know not where, But if you're a thoroughbred here, you're a thoroughbred there." - Dan Jenkins</p>
+              <p>”Here’s to lying, cheating, stealing, and drinking…If you’re going to lie, lie for a friend. If you’re going to cheat, cheat death. If you’re going to steal, steal a heart. If you’re going to drink, drink with me.”</p>
+              <p>“There are good ships, and there are wood ships, ships that sail the sea, but the best ships are friendships, and may they always be.”</p>
+              <p>"Keep your head cool and your feet warm, and a glass of good whiskey will do you no harm."</p>
+              <p>"May you live as long as you want and never want as long as you live!”</p>
+              <p>”May the best you've ever seen Be the worst you'll ever see; May a moose ne'er leave yer girnal Wi' a teardrop in his e'e. May ye aye keep hale and hearty Till ye're auld enough tae dee, May ye aye be just as happy As I wish ye aye tae be.” - Allan Ramsay of Ayr</p>
+            </div>
+          </Grid>
+          <Grid item lg={3} md={6} sm={12} xs={12}>
+            <div>
+              <RegisterForm />
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>”Life is a waste of time. Time is a waste of life. So why not get wasted all the time, and have the time of our life.”</p>
-          <p>”Here’s to lying, cheating, stealing, and drinking…If you’re going to lie, lie for a friend. If you’re going to cheat, cheat death. If you’re going to steal, steal a heart. If you’re going to drink, drink with me.”</p>
-          <p>“There are good ships, and there are wood ships, ships that sail the sea, but the best ships are friendships, and may they always be.”</p>
-          <p>"Keep your head cool and your feet warm, and a glass of good whiskey will do you no harm."</p>
-          <p>"Here's to Miguel de Cervantes who said, "I drink when I have occasion and sometimes when I have no occasion.”</p>
-          <p>"One glass is wholesome, two glasses toothsome, three glasses blithesome, four glasses fulsome, five glasses noisome, six glasses quarrelsome, seven glasses darksome."</p>
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
-
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
+              <center>
+                <h4>Already a Member?</h4>
+                <button className="btn btn_sizeSm" onClick={onLogin}>
+                  Login
+                </button>
+              </center>
+            </div>
+          </Grid>
+        </Grid>
       </div>
-    </div>
     </Paper>
   );
 }
