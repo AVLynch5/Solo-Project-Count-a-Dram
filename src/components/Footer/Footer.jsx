f7c433693d678f6fe50e34172179949099caab03
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-21 23:26:11 -0400

    applied styling to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Anthony Lynch 2021</footer>;
+  return <div className="footerBox"><footer>&copy; Anthony Lynch 2021</footer></div>;
 }
 
 export default Footer;
