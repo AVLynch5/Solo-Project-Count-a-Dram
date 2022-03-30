4639f4763ff06c1e8ae935a58f372cbaa3d936fd
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-03-30 09:58:45 -0400

    created module rearrangeArray to convert results array from range GET to a structured object

---
+++ server/modules/resultsArrToObject.js
@@ -0,0 +1,19 @@
+// function to restructure range GET results array to an object 
+
+const rearrangeArray = (initialArr) => {
+  const newObj = {};
+  for (let obj of initialArr) {
+      let localDate = new date(+obj.dram_epoch).toLocaleDateString();//this converts the epoch timestamp to the user's local date
+      if (!newObj.localDate) {
+          newObj.localDate = {"sumQuant": obj.dram_quantity, "sumCals": obj.dram_calories};
+      } else {
+          newObj.localDate.sumQuant += obj.dram_quantity;
+          newObj.localDate.sumCals += obj.dram_calories;
+      }
+  }
+  return newObj;
+};
+
+module.exports = {
+  rearrangeArray,
+};
