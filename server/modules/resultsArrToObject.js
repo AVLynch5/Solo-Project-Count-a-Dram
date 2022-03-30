4136722db7799aea784fd00b9f1565b5d983171b
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-03-30 13:34:04 -0400

    added comments to resultsArrToObject module

--- server/modules/resultsArrToObject.js
+++ server/modules/resultsArrToObject.js
@@ -4,11 +4,11 @@ const rearrangeArray = (initialArr) => {
   const newObj = {};
   for (let obj of initialArr) {
       let localDate = new Date(+obj.dram_epoch).toLocaleDateString();//this converts the epoch timestamp to the user's local date
-      if (!newObj.localDate) {
-          newObj.localDate = {"sumQuant": obj.dram_quantity, "sumCals": obj.dram_calories};
+      if (!newObj[localDate]) {
+          newObj[localDate] = {"sumQuant": obj.dram_quantity, "sumCals": obj.dram_calories};//if newObj doesnt't have a key matching localDate, add a property with key localDate and value object (containing calorie and quantity info)
       } else {
-          newObj.localDate.sumQuant += obj.dram_quantity;
-          newObj.localDate.sumCals += obj.dram_calories;
+          newObj[localDate].sumQuant += obj.dram_quantity;//if newObj has an existing key matching localDate, sum the daily dram_quantity
+          newObj[localDate].sumCals += obj.dram_calories;//if newObj has an existing key matching localDate, sum the daily dram_calories
       }
   }
   return newObj;
