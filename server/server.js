60f847d3d56e80f5a5778dbe0c6e91d92387ccd5
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-04-29 11:15:54 -0400

    added whiskeyRouter to server.js routes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const dramRouter = require('./routes/dram.router');
+const whiskeyRouter = require('./routes/whiskey.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/dram', dramRouter);
+app.use('/api/whiskey', whiskeyRouter);
 
 // Serve static files
 app.use(express.static('build'));
