a8760159b7e11ec23f8d1a2abec6a29aacd0b930
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-04-29 14:06:08 -0400

    whiskiesRouter - added GET method to fetch whiskies

--- server/routes/whiskies.router.js
+++ server/routes/whiskies.router.js
@@ -3,4 +3,22 @@ const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+//GET
+router.get('/', rejectUnauthenticated, async (req, res) => {
+    const userID = req.user.id;
+    const queryText = `
+        SELECT "whiskey"."whiskey_name", "whiskey"."whiskey_type", "whiskey"."whiskey_proof"
+        FROM "whiskey"
+        WHERE "whiskey"."user_id" = $1
+        ORDER BY "whiskey"."whiskey_name" ASC;`;
+    pool.query(queryText, [userID])
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((error) => {
+            console.log('Error GETting whiskies', error);
+            res.sendStatus(500);
+        });
+})
+
 module.exports = router;
