9f19fc7265edfd92535f83483b3f536e5dfc2b75
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-04-29 14:49:14 -0400

    updated WhiskeyList component

--- src/components/WhiskeyList/WhiskeyList.jsx
+++ src/components/WhiskeyList/WhiskeyList.jsx
@@ -1,6 +1,28 @@
 import React, {useState, useEffect} from 'React';
+import axios from 'axios';
 
 function ViewWhiskeyList (){
+    const [whiskeyListObj, setWhiskeyListObj] = useState({});
+
+    useEffect(() => {
+        fetchData();    
+    }, []);
+
+    const fetchData = async () => {
+        const result = await axios.get(`/api/whiskies`);
+        processWhiskeyList(result.data);
+    };
+
+    processWhiskeyList(array) {
+        const filterNoName = array.filter(whiskey => whiskey.whiskey_name != "");
+        const whiskeyCategories = filterNoName.reduce((nameWhiskeys, {whiskey_type, whiskey_name}) => {
+            if (!nameWhiskeys[whiskey_type]) nameWhiskeys[whiskey_type] = [];
+            nameWhiskeys[whiskey_type].push(whiskey_name);
+            return nameWhiskeys;
+        }, {});
+        setWhiskeyListObj(whiskeyCategories);
+    };
+
     return(
         <>
         </>
