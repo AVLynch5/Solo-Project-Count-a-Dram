23853cbcbff21b305c6c613511bc781ef6bd0c4a
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-05-03 19:34:52 -0400

    updated WhiskeyListChild to display whiskey lists

--- src/components/WhiskeyListChild/WhiskeyListChild.jsx
+++ src/components/WhiskeyListChild/WhiskeyListChild.jsx
@@ -1,6 +1,20 @@
 const WhiskeyListChild = ({childObj}) => {
     return(
         <>
+            {Object.entries(childObj).map(([key, value], i) => {
+                return(
+                    <div key={i}>
+                        <figure>
+                            <figcaption>{key}</figcaption>
+                            <ul>
+                                {value.map((whiskyName) => {
+                                    return <li>{whiskyName}</li>
+                                })}
+                            </ul>
+                        </figure>
+                    </div>
+                );
+            })}
         </>
     );
 };
