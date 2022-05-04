71850177a590df7a6d5da65ae3f25e88e8943ca8
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-05-03 22:50:52 -0400

    updated Constants.js with calorie calculation constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,9 @@
 const Constants = {
     whiskeyTypes: ['Scotch', 'Irish', 'Japanese', 'Canadian', 'Bourbon', 'Rye', 'Blended', 'Single Malt', 'Tennessee', 'American Wheated'],
+    alcByVolume: 200,
+    mlPerOz: 29.5735,
+    gramsPerMlAlc: 0.789,
+    calsPerGramAlc: 7
 };
 
 export default Constants;
