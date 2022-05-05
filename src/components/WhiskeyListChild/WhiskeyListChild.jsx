5c3e61275acec838adbb36d296a15dccf7c238d3
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-05-05 16:29:03 -0400

    WhiskeyListChild - added divs to style WhiskeyList component

--- src/components/WhiskeyListChild/WhiskeyListChild.jsx
+++ src/components/WhiskeyListChild/WhiskeyListChild.jsx
@@ -1,20 +1,24 @@
 const WhiskeyListChild = ({childObj}) => {
     return(
         <>
-            {Object.entries(childObj).map(([key, value], i) => {
-                return(
-                    <div key={i}>
-                        <figure>
-                            <figcaption>{key}</figcaption>
-                            <ul>
-                                {value.map((whiskyName, i) => {
-                                    return <li key={i}>{whiskyName.name}, {whiskyName.proof} proof</li>
-                                })}
-                            </ul>
-                        </figure>
-                    </div>
-                );
-            })}
+            <div className="containerDiv">
+                <div className="listDiv">
+                    {Object.entries(childObj).map(([key, value], i) => {
+                        return(
+                            <div className="cardDiv" key={i}>
+                                <figure>
+                                    <figcaption>{key}</figcaption>
+                                    <ul>
+                                        {value.map((whiskyName, i) => {
+                                            return <li key={i}>{whiskyName.name}, {whiskyName.proof} proof</li>
+                                        })}
+                                    </ul>
+                                </figure>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     );
 };
