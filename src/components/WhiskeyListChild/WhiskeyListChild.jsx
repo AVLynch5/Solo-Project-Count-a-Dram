63d588ab6fe0d828244532509c68d4416d393260
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-05-23 12:13:01 -0400

    WhiskeyListChild - added key to map function

--- src/components/WhiskeyListChild/WhiskeyListChild.jsx
+++ src/components/WhiskeyListChild/WhiskeyListChild.jsx
@@ -1,19 +1,24 @@
+import './WhiskeyListChild.css';
+
 const WhiskeyListChild = ({childObj}) => {
     return(
         <>
             <div className="containerDiv">
+                <h3>Whiskey List</h3>
                 <div className="listDiv">
                     {Object.entries(childObj).map(([key, value], i) => {
                         return(
                             <div className="cardDiv" key={i}>
-                                <figure>
-                                    <figcaption>{key}</figcaption>
-                                    <ul>
+                                <div className="cardHeader">
+                                    <h4 style={{marginBottom: 0.25}}>{key}</h4>
+                                </div>
+                                <div className='cardBody'>
+                                    <ul style={{marginTop: 10}}>
                                         {value.map((whiskyName, i) => {
                                             return <li key={i}>{whiskyName.name}, {whiskyName.proof} proof</li>
                                         })}
                                     </ul>
-                                </figure>
+                                </div>
                             </div>
                         );
                     })}
