cb8df7161d87a8d917b68e97fb8df781635548e2
Anthony Lynch <anthonyvlynch5@gmail.com>
2022-05-24 00:13:46 -0400

    modified WhiskeyList component - loading screen

--- src/components/WhiskeyList/WhiskeyList.jsx
+++ src/components/WhiskeyList/WhiskeyList.jsx
@@ -1,9 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import WhiskeyListChild from "../WhiskeyListChild/WhiskeyListChild";
+import ReactLoading from 'react-loading';
+import './WhiskeyList.css'; 
 
 function ViewWhiskeyList (){
     const [whiskeyListObj, setWhiskeyListObj] = useState({});
+    const loadingDiv = () => (
+        <div className='containerDiv'><h3 style={{marginBottom: 0.25}}>Waiting for Whiskey(s)</h3><div className='loadingDiv'><ReactLoading type={'bubbles'} color={'#FA7F43'} height={'60%'} width={'60%'} /></div></div>
+    );
 
     useEffect(() => {
         fetchData();    
@@ -16,7 +21,7 @@ function ViewWhiskeyList (){
 
     return(
         <>
-            {Object.keys(whiskeyListObj).length == 0 ? "Loading" : <WhiskeyListChild childObj={whiskeyListObj}/>}
+            {Object.keys(whiskeyListObj).length == 0 ? loadingDiv() : <WhiskeyListChild childObj={whiskeyListObj}/>}
         </>
     );
 };
